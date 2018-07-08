b8babc67ddca419b7e4c12e7e3325d71bc61501b
axj1l59 <adam_johnston1@homedepot.com>
2018-07-08 17:25:12 -0400

    Adding log levels

--- src/logger/stackdriver/index.js
+++ src/logger/stackdriver/index.js
@@ -5,17 +5,14 @@ export const createStackDriverLogger = ({
     log
 }) => () => new Promise((resolve, reject) => {
 
-    console.log('level', level)
-    console.log('log', log)
+    console.log(log)
 
     const logName = 'smart-home-monitoring-system'
     const gcpLog = logging.log(logName)
-    const metadata = {resource: {type: 'global'}}
-    const entry = gcpLog.entry(metadata, JSON.stringify(log)) 
+    const metadata = { resource: { type: 'global' } }
+    const entry = gcpLog.entry(metadata, JSON.stringify(log) )  
 
-    gcpLog
-        .write(entry)
-        // .then(resolve)
+    gcpLog['error'](entry)
         .then(() => console.log('Successfully posted log'))
         .catch(err => {
             console.error('ERROR:', err)
