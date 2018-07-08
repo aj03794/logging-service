8676fd4313810543d6391f55531c2826f561d9fc
axj1l59 <adam_johnston1@homedepot.com>
2018-07-08 17:26:21 -0400

    Adding some TODOs, removed hardcoded logging level

--- src/logger/stackdriver/index.js
+++ src/logger/stackdriver/index.js
@@ -7,12 +7,14 @@ export const createStackDriverLogger = ({
 
     console.log(log)
 
+    // TODO remove this hardcoding
     const logName = 'smart-home-monitoring-system'
     const gcpLog = logging.log(logName)
+    // TODO find out what about resources in GCP docs
     const metadata = { resource: { type: 'global' } }
     const entry = gcpLog.entry(metadata, JSON.stringify(log) )  
 
-    gcpLog['error'](entry)
+    gcpLog[level](entry)
         .then(() => console.log('Successfully posted log'))
         .catch(err => {
             console.error('ERROR:', err)
